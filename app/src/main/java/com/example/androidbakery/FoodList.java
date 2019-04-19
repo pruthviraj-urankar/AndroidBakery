b9572e551cfaa7402c186429bf5f779f0f2635b6
rakshithhv <rakshith.hv741@gmail.com>
2019-04-19 21:17:29 +0530

    Food List added

--- app/src/main/java/com/example/androidbakery/FoodList.java
+++ app/src/main/java/com/example/androidbakery/FoodList.java
@@ -1,5 +1,6 @@
 package com.example.androidbakery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
@@ -63,7 +64,9 @@ public class FoodList extends AppCompatActivity {
                 viewHolder.setItemClickListener(new ItemClickListener() {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
-                        Toast.makeText(FoodList.this,""+local.getName(),Toast.LENGTH_SHORT).show();
+                        Intent foodDetail = new Intent(FoodList.this, FoodDetail.class);
+                        foodDetail.putExtra("FoodId",adapter.getRef(position).getKey());
+                        startActivity(foodDetail);
                     }
                 });
             }
